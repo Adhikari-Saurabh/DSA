8f875ec8dfbae9cb408699746ebc85da18574098
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-07-26 14:52:47 +0545

    custome linked list

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CutomLinkedList;
+
+Console.WriteLine("Hello, World!");
+CutomLinkedList.LinkedList<string> linkedList = new CutomLinkedList.LinkedList<string>();
+
+
+Node<string> a = new Node<string>("a1");
+Node<string> b = new Node<string>("a2");
+Node<string> c = new Node<string>("a3");
+Node<string> d = new Node<string>("a4");
+Node<string> e = new Node<string>("a5");
+linkedList.AddFirst(a);
+linkedList.AddFirst(b);
+linkedList.AddFirst(c);
+linkedList.AddFirst(d);
+linkedList.AddFirst(e);
+linkedList.Traverse();
